f8cfe34acf04ae98b93b4aa15de6bb8193fb1a92
kurniawa <kurniawan_01@ymail.com>
2019-02-19 12:08:58 +0100

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,37 +1,35 @@
-
-var dbPromise = idb.open('posts-store', 1, function (db) {
+var dbPromise = idb.open('posts-store', 1, function(db) {
   if (!db.objectStoreNames.contains('posts')) {
-    db.createObjectStore('posts', {keyPath: 'id'});
+    db.createObjectStore('posts', { keyPath: 'id' });
+  } else if (!db.objectStoreNames.contains('sync-posts')) {
+    db.createObjectStore('sync-posts', { keyPath: 'id' });
   }
 });
 
 function writeData(st, data) {
-  return dbPromise
-    .then(function(db) {
-      var tx = db.transaction(st, 'readwrite');
-      var store = tx.objectStore(st);
-      store.put(data);
-      return tx.complete;
-    });
+  return dbPromise.then(function(db) {
+    var tx = db.transaction(st, 'readwrite');
+    var store = tx.objectStore(st);
+    store.put(data);
+    return tx.complete;
+  });
 }
 
 function readAllData(st) {
-  return dbPromise
-    .then(function(db) {
-      var tx = db.transaction(st, 'readonly');
-      var store = tx.objectStore(st);
-      return store.getAll();
-    });
+  return dbPromise.then(function(db) {
+    var tx = db.transaction(st, 'readonly');
+    var store = tx.objectStore(st);
+    return store.getAll();
+  });
 }
 
 function clearAllData(st) {
-  return dbPromise
-    .then(function(db) {
-      var tx = db.transaction(st, 'readwrite');
-      var store = tx.objectStore(st);
-      store.clear();
-      return tx.complete;
-    });
+  return dbPromise.then(function(db) {
+    var tx = db.transaction(st, 'readwrite');
+    var store = tx.objectStore(st);
+    store.clear();
+    return tx.complete;
+  });
 }
 
 function deleteItemFromData(st, id) {
